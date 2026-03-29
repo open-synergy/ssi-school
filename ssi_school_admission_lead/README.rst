326919b0e1134123c807bfc3b01187ff9a518477
Andhitia Rama <andhitia.r@gmail.com>
2026-03-30 00:22:09 +0700

    [ADD] ssi_school_admission_lead

---
+++ ssi_school_admission_lead/README.rst
@@ -0,0 +1,21 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+======================
+School Admission Lead
+======================
+
+Glue module between School Admission and CRM Lead.
+
+Adds ``admission_form_id`` and ``admission_test_id`` fields to ``crm.lead``,
+and provides a wizard to create an admission form directly from a lead.
+
+Credits
+=======
+
+Contributors
+------------
+
+* PT. Simetri Sinergi Indonesia
+* OpenSynergy Indonesia
---
+++ ssi_school_admission_lead/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "School Admission Lead",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_school_lead",
+        "ssi_school_admission",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/crm_lead_create_admission_form.xml",
+        "views/crm_lead.xml",
+    ],
+}
