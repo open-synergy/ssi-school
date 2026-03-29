596a00c7d213e752afb4a70ab8cdad805de7f423
Andhitia Rama <andhitia.r@gmail.com>
2026-03-30 02:23:03 +0700

    [ADD] ssi_school_admission_operating_unit

---
+++ ssi_school_admission_operating_unit/models/school_admission.py
@@ -0,0 +1,13 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SchoolAdmission(models.Model):
+    _name = "school_admission"
+    _inherit = [
+        "school_admission",
+        "mixin.single_operating_unit",
+    ]
