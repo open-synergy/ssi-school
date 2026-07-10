2c3ec1c294e11cf0be46f33d837d58c97efad733
Andhitia Rama <andhitia.r@gmail.com>
2026-07-11 06:36:30 +0700

    [UPD] ssi_school_student_graduation_operating_unit
    * Menambahkan dukungan single Operating Unit ke school_student_graduation_batch
    * Meneruskan operating_unit_id dari batch ke setiap dokumen school_student_graduation
      yang di-generate saat batch done (override _prepare_graduation_data)
    * Menambahkan security (res_groups + ir_rule) dan view OU untuk batch
    * Menambahkan unit test skenario propagasi Operating Unit batch -> graduation

--- ssi_school_student_graduation_operating_unit/models/__init__.py
+++ ssi_school_student_graduation_operating_unit/models/__init__.py
@@ -3,3 +3,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import school_student_graduation  # noqa: F401
+from . import school_student_graduation_batch  # noqa: F401
--- ssi_school_student_graduation_operating_unit/tests/__init__.py
+++ ssi_school_student_graduation_operating_unit/tests/__init__.py
@@ -3,3 +3,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_school_student_graduation_operating_unit  # noqa: F401
+from . import test_school_student_graduation_batch_operating_unit  # noqa: F401
