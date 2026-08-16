534c47222d988b3623fd9d770e60ccdb444db159
Andhitia Rama <andhitia.r@gmail.com>
2026-08-16 08:18:50 +0700

    [UPD] ssi_school_admission_lead_operating_unit
    * Tambah docstring pada class test TestCrmLeadCreateAdmissionOperatingUnit
    * Tambah docstring pada method test_crm_lead_create_admission_operating_unit
    * Menuntaskan temuan validator unit-test aturan
      setiap-class-method-test-punya-docstring

--- ssi_school_admission_lead_operating_unit/tests/test_crm_lead_create_admission_operating_unit.py
+++ ssi_school_admission_lead_operating_unit/tests/test_crm_lead_create_admission_operating_unit.py
@@ -11,7 +11,21 @@ from odoo.tests import tagged
 class TestCrmLeadCreateAdmissionOperatingUnit(
     YamlTransactionCase
 ):  # pylint: disable=too-few-public-methods
+    """Test operating unit propagation from ``crm.lead`` to admission.
+
+    Covers both admission creation wizards opened from a lead
+    (``crm.lead.create_admission_form`` and ``crm.lead.create_admission``):
+    the wizard must default ``operating_unit_id`` from the originating
+    lead, and the resulting admission document must carry that same
+    operating unit.
+    """
+
     def test_crm_lead_create_admission_operating_unit(self):
+        """Run the lead-to-admission operating unit propagation scenarios.
+
+        Asserts that the admission form and the admission created from a
+        lead inherit ``operating_unit_id`` from that lead.
+        """
         self.run_yaml_scenario(
             "test_data_crm_lead_create_admission_operating_unit.yaml"
         )
